05c5cf4edb3ac056c9a03b3f4e613d7d3b68c261
pausk <31128860+pausk@users.noreply.github.com>
2017-09-27 09:21:23 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 
+usr/sbin/sshd
+
+# Default exec
+EXEC=${EXEC:-bash}
+
+# Exec ARGS or EXEC
+exec ${@:-$EXEC}
